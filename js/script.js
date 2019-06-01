27a998e3420fa8bb787d7c22371d204409953c94
Łukasz Nolka <nolkalukasz@gmail.com>
2019-06-01 14:54:54 +0200

    Add media-queries for our-projects section

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.querySelector('.project-dropdown').addEventListener('click', (e) => {
+    document.querySelector('.projects-categories').classList.toggle('show');
+});
+document.querySelector('.projects-categories').addEventListener('click', (e) => {
+    document.querySelector('.projects-categories').classList.toggle('show');
+});
